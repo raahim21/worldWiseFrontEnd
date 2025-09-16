ac22a41ecf14956b425cef71b37bc3c2a45c4136
Raahim Wajid Ali <Raahimwajid21@gmail.com>
2025-09-16 14:39:24 +0500

    toastify debug signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,3 @@
-// import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
-
-// import CountryList from "./components/CountryList";
-// import Form from "./components/Form";
-// import Homepage from "./pages/Homepage";
-// import Product from "./pages/Product";
-// import Pricing from "./pages/Pricing";
-// import PageNotFound from "./pages/PageNotFound";
-// import Login from "./pages/Login";
-// import AppLayout from "./pages/AppLayout";
-// import CityList from "./components/CityList";
-// import City from "./components/City";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import "./index.css";
-// import { useAuth } from "./contexts/AuthContext";
-// import Signup from "./pages/Signup";
-
-// function App() {
-//   let { user } = useAuth();
-//   console.log(user);
-//   return (
-//     <BrowserRouter>
-//       <ToastContainer position="top-right" autoClose={3000} />
-//       <Routes>
-//         <Route index element={<Homepage />} />
-//         <Route path="product" element={<Product />} />
-//         <Route path="pricing" element={<Pricing />} />
-//         <Route path="app" element={!user ? <Login /> : <AppLayout />}>
-//           <Route index element={<Navigate replace to="cities" />} />
-//           <Route path="cities" element={<CityList />} />
-
-//           <Route path="cities/:id" element={<City />} />
-//           <Route path="countries" element={<CountryList />} />
-//           <Route path="form" element={<Form />} />
-//         </Route>
-//         <Route
-//           path="login"
-//           element={user ? <Navigate replace to="/app" /> : <Login />}
-//         />
-//         <Route
-//           path="signup"
-//           element={user ? <Navigate replace to="/app" /> : <Signup />}
-//         />
-//         <Route path="*" element={<PageNotFound />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -120,7 +68,11 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-      <ToastContainer position="top-right" autoClose={3000} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        style={{ zIndex: 9999 }}
+      />
     </>
   );
 }
